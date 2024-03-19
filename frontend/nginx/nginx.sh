bd6bad1decf38b919c167241bcf794505c7cf7c3
Matejusz <smetmateusz@gmail.com>
2024-03-19 08:45:51 +0100

    Diched the VUE_API from env in frontend

--- frontend/nginx/nginx.sh
+++ frontend/nginx/nginx.sh
@@ -4,5 +4,5 @@ set -e
 # envsub does not overwrite the data in default and just adding it is not enough
 rm /etc/nginx/conf.d/default.conf
 # substitute the backend info 
-envsubst '$VUE_APP_API_HOST $VUE_APP_API_PORT' < /app/nginx/nginx-app.conf > /etc/nginx/conf.d/default.conf
+envsubst '$API_HOST $API_PORT' < /app/nginx/nginx-app.conf > /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
